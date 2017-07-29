8bbb02a361b823ed277fa50a5d4f423a01152bd4
Challen <273239067@qq.com>
2017-07-29 09:22:38 +0800

    java design model's proxy

---
+++ src/JAVA_Design_Model_Proxy/ProxyTest.java
@@ -0,0 +1,11 @@
+package JAVA_Design_Model_Proxy;
+
+public class ProxyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sourceable source = new Proxy();
+		source.method(); 
+	}
+
+}
---
+++ src/JAVA_Design_Model_Proxy/Sourceable.java
@@ -0,0 +1,5 @@
+package JAVA_Design_Model_Proxy;
+
+public interface Sourceable {
+	public void method();
+}
